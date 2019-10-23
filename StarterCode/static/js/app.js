7ffb8f8c5f82dff9883b5ce4c40003a9bed10892
tonitiri <taiwo@beyondbordersinvestment.com>
2019-10-23 14:09:20 -0500

    homework80%

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,43 +6,60 @@ console.log(data);
 var table = d3.select("#ufo-table");
 var tableBody = table.select("tbody");
 
+function buildTable(ufo_data) {
+    tableBody.html("")
+    ufo_data.forEach(ufo => {
+        var row = tableBody.append("tr");
 
-data.forEach(ufo => {
-    var row = tableBody.append("tr");
+        row.append("td").text(ufo.datetime);
+        row.append("td").text(ufo.city);
+        row.append("td").text(ufo.state);
+        row.append("td").text(ufo.country);
+        row.append("td").text(ufo.shape);
+        row.append("td").text(ufo.durationMinutes);
+        row.append("td").text(ufo.comments);
 
-    row.append("td").text(ufo.datetime);
-    row.append("td").text(ufo.city);
-    row.append("td").text(ufo.state);
-    row.append("td").text(ufo.country);
-    row.append("td").text(ufo.shape);
-    row.append("td").text(ufo.durationMinutes);
-    row.append("td").text(ufo.comments);
+    })
 
-})
+}
+buildTable(data);
 
 var characterDateField = d3.select("#datetime");
 var characterFilterButton = d3.select("#filter-btn");
 
+// var resetButton = d3.select("#resetAllButton");
+
+// resetButton.on("click", () => {
+//     tableBody.remove();
+//     tableBody = table.append("tbody");
+//     buildTable(data);
 
 
 
-characterDateField.on("click", () => {
-    var characterDateField = characterSearchField.property("value");
-    console.log(characterToSearchFor);
-    tableBody.remove();
-    tableBody = table.append("tbody");
 
-    data.filter(f => f.displayNameEn.toLowerCase().trim() == characterToSearchFor.toLowerCase().trim())
-        .forEach(data => {
-            var row = tableBody.append("tr");
+characterFilterButton.on("click", () => {
+    d3.event.preventDefault()
+    var characterDatevalue = characterDateField.property("value");
+    console.log(characterDatevalue);
 
-            row.append("td").text(ufo.datetime);
-            row.append("td").text(ufo.city);
-            row.append("td").text(ufo.state);
-            row.append("td").text(ufo.country);
-            row.append("td").text(ufo.shape);
-            row.append("td").text(ufo.durationMinutes);
-            row.append("td").text(ufo.comments);
-        });
+    var filterData = data.filter(ufo => ufo.datetime == characterDatevalue)
+
+    buildTable(filterData)
+
+    // tableBody.remove();
+    // tableBody = table.append("tbody");
+
+    // data.filter(ufo => ufo.datetime.toLowerCase().trim() == characterToSearchFor.toLowerCase().trim())
+    //     .forEach(data => {
+    //         var row = tableBody.append("tr");
+
+    //         row.append("td").text(ufo.datetime);
+    //         row.append("td").text(ufo.city);
+    //         row.append("td").text(ufo.state);
+    //         row.append("td").text(ufo.country);
+    //         row.append("td").text(ufo.shape);
+    //         row.append("td").text(ufo.durationMinutes);
+    //         row.append("td").text(ufo.comments);
+    //     });
 
 })
